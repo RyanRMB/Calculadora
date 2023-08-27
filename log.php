03c336186ce4c3ba667c55f423030079309cbfb2
RyanRMB <142849567+RyanRMB@users.noreply.github.com>
2023-08-27 11:02:45 -0300

    Add files via upload

---
+++ log.php
@@ -0,0 +1,7 @@
+<?PHP
+	function logaritmo($v1, $v2)
+	{
+		$result = log($v1, $v2);
+		return $result;
+	}
+	?>
---
+++ logdez.php
@@ -0,0 +1,7 @@
+<?PHP
+	function logdez($v1)
+	{
+		$result = log10($v1);
+		return $result;
+	}
+	?>
